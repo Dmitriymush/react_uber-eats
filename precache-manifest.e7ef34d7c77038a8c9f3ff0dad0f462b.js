05c16da20c930548d221338a7e0c414d806dee23
Dmitriymush <powder322@gmail.com>
2019-11-21 21:49:33 +0200

    Updates

---
+++ precache-manifest.e7ef34d7c77038a8c9f3ff0dad0f462b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c668dd63121ae7658ff4cfac3cbb0c2",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "c0e24699fc4f6cd3a720",
+    "url": "/react_uber-eats/static/css/main.b72ab467.chunk.css"
+  },
+  {
+    "revision": "61e63a0fc74d8b4d2a7c",
+    "url": "/react_uber-eats/static/js/2.f6be08f8.chunk.js"
+  },
+  {
+    "revision": "c0e24699fc4f6cd3a720",
+    "url": "/react_uber-eats/static/js/main.df763099.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
